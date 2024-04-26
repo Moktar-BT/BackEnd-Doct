b3c7c280de96e06da6adca6cc27d18823176875f
Moktar-BT <137128511+Moktar-BT@users.noreply.github.com>
2024-04-26 19:36:51 +0200

    create the project

---
+++ BackEndDoctApp/src/main/java/com/example/BackEndDoctApp/BackEndDoctAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackEndDoctApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndDoctAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndDoctAppApplication.class, args);
+	}
+
+}
---
+++ BackEndDoctApp/src/test/java/com/example/BackEndDoctApp/BackEndDoctAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackEndDoctApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndDoctAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

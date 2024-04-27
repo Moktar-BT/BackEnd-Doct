b4dbef61e32682df8ea3676add7b523b21f23f57
Moktar-BT <137128511+Moktar-BT@users.noreply.github.com>
2024-04-27 13:10:20 +0200

    service layer

---
+++ BackEndDoctApp/src/main/java/com/example/BackEndDoctApp/service/PatientService.java
@@ -0,0 +1,7 @@
+package com.example.BackEndDoctApp.service;
+
+import com.example.BackEndDoctApp.dto.PatientDto;
+
+public interface PatientService {
+    PatientDto createPatient( PatientDto patientDto);
+}
---
+++ BackEndDoctApp/src/main/java/com/example/BackEndDoctApp/service/impl/PatientServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.BackEndDoctApp.service.impl;
+
+import com.example.BackEndDoctApp.dto.PatientDto;
+import com.example.BackEndDoctApp.service.PatientService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientServiceImpl implements PatientService {
+
+    @Override
+    public PatientDto createPatient(PatientDto patientDto) {
+        return null;
+    }
+}

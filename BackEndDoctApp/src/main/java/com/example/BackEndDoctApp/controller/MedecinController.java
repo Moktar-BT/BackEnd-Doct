b2e6b3b7136e41ca0064f976aba5b8351f1ed07f
Moktar-BT <137128511+Moktar-BT@users.noreply.github.com>
2024-05-06 00:06:55 +0200

    link the frontpage with DB

--- BackEndDoctApp/src/main/java/com/example/BackEndDoctApp/controller/MedecinController.java
+++ BackEndDoctApp/src/main/java/com/example/BackEndDoctApp/controller/MedecinController.java
@@ -2,26 +2,29 @@ package com.example.BackEndDoctApp.controller;
 
 import com.example.BackEndDoctApp.dto.MedecinDto;
 import com.example.BackEndDoctApp.service.MedecinService;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
 @RestController
 @RequestMapping("/api/medecins")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MedecinController {
-    private MedecinService medecinService;
 
-    //Build Add Patient Rest API
+    private final MedecinService medecinService;
+
+    public MedecinController(MedecinService medecinService) {
+        this.medecinService = medecinService;
+    }
+
+    // Build Add Medecin Rest API
     @PostMapping
-    public ResponseEntity<MedecinDto> createMedecin(@RequestBody MedecinDto medecinDto){
+    public ResponseEntity<MedecinDto> createMedecin(@RequestBody MedecinDto medecinDto) {
         MedecinDto savedMedecin = medecinService.createMedecin(medecinDto);
         return new ResponseEntity<>(savedMedecin, HttpStatus.CREATED);
     }
-
-
 }
--- BackEndDoctApp/src/main/java/com/example/BackEndDoctApp/controller/PatientController.java
+++ BackEndDoctApp/src/main/java/com/example/BackEndDoctApp/controller/PatientController.java
@@ -2,26 +2,29 @@ package com.example.BackEndDoctApp.controller;
 
 import com.example.BackEndDoctApp.dto.PatientDto;
 import com.example.BackEndDoctApp.service.PatientService;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
 @RestController
 @RequestMapping("/api/patients")
+@CrossOrigin(origins = "http://localhost:5173")
 public class PatientController {
-    private PatientService patientService;
 
-    //Build Add Patient Rest API
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    // Build Add Patient Rest API
     @PostMapping
-    public ResponseEntity<PatientDto> createPatient(@RequestBody PatientDto patientDto){
+    public ResponseEntity<PatientDto> createPatient(@RequestBody PatientDto patientDto) {
         PatientDto savedPatient = patientService.createPatient(patientDto);
         return new ResponseEntity<>(savedPatient, HttpStatus.CREATED);
     }
-
-
 }
